7bff02a88ff8ae7d33c389a5c4557f1fbe1f58c0
Debbie_Codes <deborah.onchari20@gmail.com>
2025-11-07 10:52:24 +0300

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+   return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+  
+
+export default App;
---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+    return < UserInfo />;
+}
+
+export default ProfilePage;
+
+
+
