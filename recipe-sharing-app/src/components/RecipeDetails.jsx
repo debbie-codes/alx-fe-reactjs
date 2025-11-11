291c9869290b9542f134222e73fdccb38a334845
Debbie_Codes <deborah.onchari20@gmail.com>
2025-11-11 23:07:45 +0300

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
-import { useRecipeStore } from "../recipeStore";
+import { useRecipeStore } from "./recipeStore";
 import DeleteRecipeButton from "./DeleteRecipeButton";
 
 const RecipeDetails = () => {
