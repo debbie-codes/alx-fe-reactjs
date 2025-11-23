caa9a0e0ceb79367f7d002961c0bb1f364d6ca8e
Debbie_Codes <deborah.onchari20@gmail.com>
2025-11-23 14:55:01 +0300

    Updated API

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
---
+++ github-user-search/src/components/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WelcomeMessage() {
+  return <p>App setup successful-start building your components!</p>;
+}
+
+export default WelcomeMessage;
